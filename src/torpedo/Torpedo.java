f3ef2a5198d36a085f146ef6cb71f48326012723
dekanymate <dekanymate@gmail.com>
2022-05-23 13:19:29 +0200

    lovesekSzama,talalatokSzama

--- src/torpedo/Torpedo.java
+++ src/torpedo/Torpedo.java
@@ -1,8 +1,28 @@
 package torpedo;
+
+import java.util.Scanner;
+
 public class Torpedo {
 
     public static void main(String[] args) {
-        
+        int random = Math.random();
+        Hajo hajo = new Hajo(new int[]{1,3,4});
+        System.out.println("Torpedó játék");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Lövés helye: ");
+        int loves = sc.nextInt();
+        int lovesekSzama = 0, talalatokSzama = 0;
+        for (int i = 0; i < Hajo.pozicio.length; i++) {
+            if(loves==Hajo.pozicio[i]){
+                System.out.println("Talált!");
+                lovesekSzama++;
+                talalatokSzama++;
+            }else{
+                System.out.println("Nem talált!");
+                lovesekSzama++;
+            }
+            
+        }
     }
     
 }
